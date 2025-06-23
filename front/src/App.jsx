b2c31bf7c60c97f6812a36bf2d4c48111cbe91f5
Thiago <thiagosmorato@gmail.com>
2025-06-23 12:06:31 -0300

    Refactor caption generation logic; integrate ImageCaptioner class and update button functionality

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import { generateCaption } from "./models/api";
 
 function App() {
   const [imgSrc, setImgSrc] = useState(null);
   const [caption, setCaption] = useState("<Caption />");
 
-  function generateCaption() {
-    if (!imgSrc) {
-      setCaption("Please enter a valid image URL.");
-      return;
-    }
-    setCaption("Generating caption...");
+  function addCaption() {
+    const caption = generateCaption(imgSrc);
+
+    setCaption(caption);
   }
 
   return (
@@ -18,7 +17,7 @@ function App() {
       <h1>Caption Generator</h1>
       <div class='url-form'>
         <input onChange={(e) => setImgSrc(e.target.value)} />
-        <button onClick={generateCaption}>Generate</button>
+        <button onClick={addCaption}>Generate</button>
       </div>
       <div class='captioned-image'>
         <img height={200} src={imgSrc} alt='' style={{ marginBottom: "10px" }} />
