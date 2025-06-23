568b4a9260428306ec883c6278e0e3568fef703d
Thiago <thiagosmorato@gmail.com>
2025-06-23 12:02:04 -0300

    Implement image URL input and caption generation functionality

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [imgSrc, setImgSrc] = useState(null);
+  const [caption, setCaption] = useState("<Caption />");
+
+  function generateCaption() {
+    if (!imgSrc) {
+      setCaption("Please enter a valid image URL.");
+      return;
+    }
+    setCaption("Generating caption...");
+  }
+
   return (
     <>
       <h1>Caption Generator</h1>
       <div class='url-form'>
-        <input type='text' />
-        <button>Generate</button>
+        <input onChange={(e) => setImgSrc(e.target.value)} />
+        <button onClick={generateCaption}>Generate</button>
       </div>
       <div class='captioned-image'>
-        <img height={200} width={200} src='' alt='' style={{ marginBottom: "10px" }} />
-        <span>Caption</span>
+        <img height={200} src={imgSrc} alt='' style={{ marginBottom: "10px" }} />
+        <span>{caption}</span>
       </div>
     </>
   );
